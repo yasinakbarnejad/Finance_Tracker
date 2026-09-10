2d73247724270e158f6cea4661b55f1262a53fde
yasinakbarnejad <yasinphilosopher@gmail.com>
2026-09-10 13:43:13 +0430

    added login and sign up

--- server.py
+++ server.py
@@ -1,5 +1,86 @@
-from fastapi import FastAPI
+import fastapi
+from datetime import datetime
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse 
+import uvicorn
+from user import User
+from intraction import investment, transaction
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
 
+fronts = Jinja2Templates(directory="fronts")
+app = fastapi.FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+main_user = None
+@app.get("/" , response_class=HTMLResponse)
+def home(request: fastapi.Request):
+    return fronts.TemplateResponse(request,"main.html",
+    {
+        "response" : "success"
+    })
+@app.get("/home")
+def main(request: fastapi.Request):
+    return fronts.TemplateResponse(request,"main.html",
+    {
+        "request" : request,
+        "user" : main_user
+    })
+
+@app.get("/contact", response_class=HTMLResponse)
+def contact_page(request: fastapi.Request, error: str | None = None):
+    return fronts.TemplateResponse(request,"contactus.html",
+    {
+        "request" : request,
+        "user" : main_user
+    })
+@app.get("/login", response_class=HTMLResponse)
+def login_page(request: fastapi.Request, error: str | None = None):
+    return fronts.TemplateResponse(
+        request,"login.html", {"user": None, "error": error}
+    )
+    
+@app.get("/signup", response_class=HTMLResponse)
+def signup_page(request: fastapi.Request, error: str | None = None):
+    return fronts.TemplateResponse(
+        request,"signup.html", {"user": None, "error": error}
+    )
+    
+@app.post("/login")
+def login(request:fastapi.Request,
+          username: str = fastapi.Form(...),
+          password: str = fastapi.Form(...)):
+    if (not username or not password):
+        return fronts.TemplateResponse(request,"login.html", {"request":request, "error":"invalid input"})
+    user = User.find_user(username)
+    if not user:
+        print("not found")
+        return fronts.TemplateResponse(request,"login.html", {"request":request, "error":"user is not found"})
+    if user.password != password:
+        print("wrong pass")
+        return fronts.TemplateResponse(request,"login.html", {"request":request, "error":"password is wrong"})
+    global main_user
+    main_user = user
+    return RedirectResponse("/home", status_code=303)
+
+@app.post("/signup")
+def signup(request:fastapi.Request,
+          fname: str = fastapi.Form(...),
+          sname: str = fastapi.Form(...),
+          username: str = fastapi.Form(...),
+          repassword: str = fastapi.Form(...),
+          password: str = fastapi.Form(...),
+          net_worth: int = fastapi.Form(...)):
+    if (net_worth < 0):
+        return fronts.TemplateResponse(request,"signup.html", {"request":request, "error":"your net worth cant be negative"})
+    user = User.find_user(username)
+    if user:
+        return fronts.TemplateResponse(request,"signup.html", {"request":request, "error":"user already exists"})
+    if password != repassword:
+        return fronts.TemplateResponse(request,"login.html", {"request":request, "error":"passwords dont match"})
+    
+    global main_user
+    main_user = User(fname,sname,username,password,net_worth)
+    main_user.add_file()
+    return RedirectResponse("/home", status_code=303)
 
--- user.py
+++ user.py
@@ -1,10 +1,14 @@
 from datetime import date
 from intraction import transaction
 import pandas as pd
+import json
 class User:
-    def __init__(self, first_name, last_name, password,net_worth):
+    address = "data/users.json"
+    def __init__(self, first_name, last_name,username, password,net_worth):
         self.first = first_name
         self.last = last_name
+        self.username = username
+        self.password = password
         self.net_worth = net_worth
         self.investments = []
         self.last_login = date.today()
@@ -27,6 +31,42 @@ class User:
         self.calculate_investment()
         self.last_login = date.today()
     
+    @classmethod
+    def find_user(cls, username):
+        try:
+            with open(cls.address,"r") as f:
+                users =  json.load(f)["users"]
+        except FileNotFoundError:
+            users = []
+        if not users:
+            return {}
+        for user in users:
+            if user["username"]==username:
+                return User.read_dict(user) 
+        else:
+            return {} 
+    @staticmethod
+    def read_dict(user_dict):
+        new_user = User(user_dict["first_name"],user_dict["last_name"],user_dict["username"],
+                        user_dict["password"],user_dict["net_worth"])
+        return new_user
+    
+    def add_file(self):
+        new_user = {"first_name":self.first,"last_name":self.last,"username":self.username,
+                    "password":self.password, "net_worth":self.net_worth}
+        try:
+            with open(User.address,"r") as f:
+                old_dict = json.load(f)
+                users = old_dict["users"]
+        except FileNotFoundError:
+            old_dict = {"users":[]}
+            users = []
+        
+        users.append(new_user)
+        old_dict["users"] = users
+        with open(User.address, "w") as f:
+            json.dump(old_dict,f, indent=4)
+        
     def read_file(self):
         df = pd.read_csv(self.db_address)
         return df   
